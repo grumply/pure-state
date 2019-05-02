9c4260efef353ec7fba2d0d1707675b639bfe4f4
grumply <sean@grump.ly>
2019-05-02 10:26:13 -0500

    Refine interface.

--- src/Pure/State.hs
+++ src/Pure/State.hs
@@ -1,14 +1,13 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, ImplicitParams, MagicHash, FlexibleInstances, MultiParamTypeClasses, RankNTypes, PatternSynonyms #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, ImplicitParams, MagicHash, FlexibleInstances, MultiParamTypeClasses, RankNTypes, PatternSynonyms, FunctionalDependencies, UndecidableInstances #-}
 module Pure.State
   ( -- * Stateful View Monad
     PureM(..), PureRef, Reactive(..)
+  , MonadSRef(..)
   , runPureWith, runPure, runPureDyn
   -- * Lift Pure computations
   , liftPure, embedPure
-  -- * Convenience utilities
-  , withSRef
   -- * State Reference Utilities
-  , SRef(..), askSRef, readSRef, writeSRef, modifySRef
+  , SRef(..), readSRef, writeSRef, modifySRef
   -- * Stateful View Construction Combinators
   , (<$|), (=<|), (|>=), (=<||>), (=<||>=), (|#>=), (=<||#>=), (=<||#>)
   ) where
@@ -25,60 +24,104 @@ import Control.Monad
 import Control.Monad.Fix
 import Control.Monad.Trans
 import Control.Monad.IO.Class
-import qualified Control.Monad.Reader as Reader
-import qualified Control.Monad.State as State
+import Control.Monad.Reader as Reader
+import Control.Monad.State as State
 
 import Data.IORef
 import Data.Typeable
 
 import GHC.Exts
 
+-- | A reference to some mutable state `s` with an extensible write effect that
+-- enables implicitly reactive views produced in monadic contexts.
+--
+-- The `SRef` serves a dual purpose: 
+--
+--  1. Code that uses the state to produce a view must be able to modify the 
+--     state without causing re-evaluation of the view-producing code which
+--     could cause possible unintended looping of the view-producing code.
+--
+--  2. Produced views must be able to inject changes back into the reference 
+--     and force re-evaluation of the view-producing code.
+--
 data SRef s = SRef
-  { sref   :: IORef s
-  , reader :: IO s
-  , writer :: s -> IO ()
+  { ref   :: IORef s
+  , write :: s -> IO ()
   }
 
+{-# INLINE readSRef #-}
+-- | Read the current state of an SRef in a MonadIO context.
+readSRef :: MonadIO m => SRef s -> m s
+readSRef r = liftIO (readIORef (ref r))
+
+{-# INLINE writeSRef #-}
+-- | Write the current state of an SRef in a MonadIO context and force 
+-- re-evaluation of the view managing the SRef. 
+--
+-- Choose `writeSRef` for effect handlers within `View`s and `writeState`
+-- within view-producing code, like `PureM`.
+--
+-- NOTE: Using `writeSRef` within view-producing code is likely to cause 
+--       unintended looping.
+--
+writeSRef :: MonadIO m => SRef s -> s -> m ()
+writeSRef ref s = liftIO (write ref s)
+
+{-# INLINE modifySRef #-}
+-- | Modify the current state of an SRef in a MonadIO context and force 
+-- re-evaluation of the view managing the SRef. 
+--
+-- Choose `modifySRef` for effect handlers within `View`s and `modifyState`
+-- within view-producing code, like `PureM`.
+--
+-- NOTE: Using `modifySRef` within view-producing code is likely to cause 
+--       unintended looping.
+modifySRef :: MonadIO m => SRef s -> (s -> s) -> m ()
+modifySRef r f = liftIO (readIORef (ref r) >>= \s -> write r (f s))
+
 newtype PureM s a = PureM { unPureM :: Reader.ReaderT (SRef s) IO a }
-  deriving (Functor,Applicative,Monad,Alternative,MonadPlus,Reader.MonadReader (SRef s))
+  deriving (Functor,Applicative,Monad,Alternative,MonadPlus)
 
 instance MonadIO (PureM s) where
   {-# INLINE liftIO #-}
   liftIO = PureM . liftIO
 
-instance State.MonadState s (PureM s) where
-  {-# INLINE get #-}
-  get = askSRef >>= readSRef
-  {-# INLINE put #-}
-  put s = askSRef >>= \sr -> liftIO ( writeIORef (sref sr) s )
-
 instance MonadFix (PureM s) where
   {-# INLINE mfix #-}
   mfix = PureM . mfix . (unPureM .)
 
-{-# INLINE askSRef #-}
-askSRef :: PureM s (SRef s)
-askSRef = PureM Reader.ask
+class MonadIO m => MonadSRef s m | m -> s where
+  -- | Access an SRef in an `m` context.
+  sref :: m (SRef s)
 
-{-# INLINE readSRef #-}
-readSRef :: MonadIO m => SRef s -> m s
-readSRef ref = liftIO (reader ref)
+  -- | Get the current state of an SRef.
+  getState :: m s
+  getState = sref >>= readSRef
 
-{-# INLINE writeSRef #-}
--- NOTE: Using `writeSRef` within a `PureM` could cause a loop. Use
--- `put` where possible.
-writeSRef :: MonadIO m => SRef s -> s -> m ()
-writeSRef ref s = liftIO (writer ref s)
+  -- | Set the state of an SRef in a context containing access to the SRef.
+  -- This will generally be in view-producing code and the update will be 
+  -- available to any code executed after this modification, but will not force
+  -- any previously executed code to re-evaluate, unlike `writeSRef` which 
+  -- queues an update for re-evaluation.
+  setState :: s -> m ()
+  setState s = sref >>= \sr -> liftIO $ writeIORef (ref sr) s
 
-{-# INLINE modifySRef #-}
--- NOTE: Using `modifySRef` within a `PureM` could cause a loop. Use
--- `modify` where possible.
-modifySRef :: MonadIO m => SRef s -> (s -> s) -> m ()
-modifySRef ref f = liftIO (reader ref >>= \s -> writer ref (f s))
+  -- | Modify the state of an SRef in a context containing access to the SRef.
+  -- This will generally be in view-producing code and the update will be 
+  -- available to any code executed after this modification, but will not force
+  -- any previously executed code to re-evaluate, unlike `modifySRef` which 
+  -- queues an update for re-evaluation.
+  modifyState :: (s -> s) -> m ()
+  modifyState f = sref >>= \sr -> liftIO $ modifyIORef (ref sr) f
 
-{-# INLINE withSRef #-}
-withSRef :: (SRef s -> IO a) -> PureM s a
-withSRef f = askSRef >>= liftIO . f
+  {-# MINIMAL sref #-}
+
+instance MonadSRef s (PureM s) where
+  sref = PureM Reader.ask
+
+instance (MonadIO m, MonadSRef s m) => MonadState s m where
+  get = getState
+  put = setState
 
 data Reactive = Reactive
   { onState :: Bool
@@ -102,7 +145,7 @@ runPureWith dyn s p = flip Component (dyn,s,p) $ \self ->
       { construct = do
           (dyn,s,p) <- Pure.ask self
           s_ <- newIORef s
-          let stateRef = SRef s_ (readIORef s_) (\s -> writeIORef s_ s >> updateView)
+          let stateRef = SRef s_ (\s -> writeIORef s_ s >> updateView)
           view <- eval p stateRef
           return (s,p,view,stateRef)
       , receive = \(new_dyn,new_s,new_p) (s,p,view,stateRef) -> do
@@ -119,7 +162,7 @@ runPureWith dyn s p = flip Component (dyn,s,p) $ \self ->
             p' = if injectpure  then new_p else p
 
           when injectstate 
-            ( writeIORef ( sref stateRef ) new_s )
+            ( writeIORef ( ref stateRef ) new_s )
 
           if injectstate || injectpure then do
             view <- eval p' stateRef
@@ -132,21 +175,21 @@ runPureWith dyn s p = flip Component (dyn,s,p) $ \self ->
 
 {-# INLINE liftPure #-}
 liftPure :: MonadIO m => SRef s -> PureM s a -> m a
-liftPure sref pm = liftIO $ do
+liftPure s pm = liftIO $ do
   let wrapped = do
         st0 <- State.get
         a <- pm
         st1 <- State.get
         unless ( isTrue# ( reallyUnsafePtrEquality# st0 st1 ) )
-          ( writeSRef sref st1 )
+          ( writeSRef s st1 )
         return a
-  Reader.runReaderT ( unPureM wrapped ) sref
+  Reader.runReaderT ( unPureM wrapped ) s
 
 {-# INLINE embedPure #-}
-embedPure :: MonadIO m => PureM s a -> PureM s (m a)
+embedPure :: (MonadSRef s m, MonadIO m, MonadIO n) => PureM s a -> m (n a)
 embedPure m = do
-  sref <- askSRef
-  pure ( liftPure sref m )
+  s <- sref
+  pure ( liftPure s m )
 
 {-# INLINE runPure #-}
 runPure :: Typeable s => s -> PureM s View -> View
