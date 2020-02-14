1e848bf467d7ffa2f9f02ee435ffc317f19ff1e5
Sean Hickman <sean@grumply.com>
2020-02-13 23:35:59 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, exceptions, mtl, pure-core, pure-default, pure-dom, pure-lifted, stdenv, transformers }:
 mkDerivation {
   pname = "pure-state";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base exceptions mtl pure-core pure-default pure-dom pure-lifted transformers ];
   homepage = "github.com/grumply/pure-state";
